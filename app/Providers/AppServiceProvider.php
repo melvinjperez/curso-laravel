9209a603c27fea8210c9b6e5f9a765aceda0dbe3
Melvin Pérez <melvin.perez@outlook.com>
2020-06-01 18:58:43 -0600

    Implementación AdminLTE & Blade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Facade\FlareClient\View;
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -13,7 +13,49 @@
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="{{asset("assets/$theme/dist/css/adminlte.min.css")}}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
+<body class="hold-transition sidebar-mini layout-boxed">
+  <!-- Site wrapper -->
+  <div class="wrapper">
+    <!-- Inicio Header --> 
+    @include("theme/$theme/header")
+    <!-- File Header --> 
+    <!-- Inicio Aside --> 
+    @include("theme/$theme/aside")
+    <!-- File Aside --> 
+        
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+      <section class="content"> 
+        <div class="card-header">
+          <h3 class="card-title">Biblioteca</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+              <i class="fas fa-minus"></i></button>            
+          </div>
+        </div>
+        <div class="card-body">
+          Start creating your amazing application!
+        </div>
+        <!-- /.card-body -->  
+      </section>
+    </div> 
+    <!-- Inicio Footer -->
+    @include("theme/$theme/footer")
+    <!-- File Footer --> 
+  </div>
+  <!-- jQuery -->
+  <script src="{{asset("assets/$theme/plugins/jquery/jquery.min.js")}}"></script>
+  <!-- Bootstrap 4 -->
+  <script src="{{asset("assets/$theme/plugins/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+  <!-- AdminLTE App -->
+  <script src="{{asset("assets/$theme/dist/js/adminlte.min.js")}}"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="{{asset("assets/$theme/dist/js/demo.js")}}"></script>
+</body>
+</html>
